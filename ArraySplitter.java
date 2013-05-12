1cb9491679c622ded49c048b3f849af7270e7a66
Petter Hoel <petter@petterhoel.net>
2013-05-12 13:36:46 +0200

    merge class implemented

--- ArraySplitter.java
+++ ArraySplitter.java
@@ -2,10 +2,9 @@ import java.util.Arrays;
 
 class ArraySplitter{
 	String[][] values;
-	int length;
 
 	ArraySplitter(String[] words){
-		length = words.length/2;
+		int length = words.length/2;
 		values = new String[][] {Arrays.copyOfRange(words, 0, length), 
 								 Arrays.copyOfRange(words, length, words.length)};
 	}
@@ -17,12 +16,12 @@ class ArraySplitter{
 
 
 	//test
-	/*public void print(){
+	public void print(){
 		System.out.println(values.length);
 		System.out.println("Array one: " + values[0].length);
 		for (int i = 0; i < values[0].length; i++) System.out.println(values[0][i]);
 		System.out.println("Array two: " + values[1].length);
 		for (int i = 0; i < values[1].length; i++) System.out.println(values[1][i]);
-	}*/
+	}
 	
 }
--- Merge.java
+++ Merge.java
@@ -1,3 +1,19 @@
 class Merge{
-	
+	String [] merged;
+
+	Merge(String[][] toMerge){
+		merged = merge(toMerge);
+	}
+
+	public String[] merge(){
+		return merged;
+	}
+
+	private String[] merge(String[][] toMerge){
+		int size  = toMerge[0].length + toMerge[1].length;
+		BinaryTree bt = new BinaryTree();
+		for (int i = 0; i < toMerge[0].length; i++) bt.insert(toMerge[0][i]);
+		for (int i = 0; i < toMerge[1].length; i++) bt.insert(toMerge[1][i]);
+		return bt.toArray(size);
+	}
 }
