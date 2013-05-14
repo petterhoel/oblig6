5476acad5fd1711007785b0755328da5fcbf6892
Petter Hoel <petter@petterhoel.net>
2013-05-14 16:23:19 +0200

    minor stuff

--- sort.sh
+++ sort.sh
@@ -7,21 +7,21 @@ then
 echo "compilation successful, running program"
 
 
-java Sort 1 Files/names.txt SortedNames.txt
-java Sort 5 Files/names.txt SortedNames.txt
-java Sort 3000 Files/names.txt SortedNames.txt
+#java Sort 1 Files/names.txt SortedNames.txt
+#java Sort 5 Files/names.txt SortedNames.txt
+#java Sort 3000 Files/names.txt SortedNames.txt
 
 
 
-java Sort 1 Files/sowpods.txt SortedNames.txt
+#java Sort 1 Files/sowpods.txt SortedNames.txt
 java Sort 5 Files/sowpods.txt SortedNames.txt
-java Sort 3000 Files/sowpods.txt SortedNames.txt
+#java Sort 3000 Files/sowpods.txt SortedNames.txt
 
 
 
-java Sort 1 Files/linuxwords.txt SortedNames.txt
-java Sort 5 Files/linuxwords.txt SortedNames.txt
-java Sort 3000 Files/linuxwords.txt SortedNames.txt
+#java Sort 1 Files/linuxwords.txt SortedNames.txt
+#java Sort 5 Files/linuxwords.txt SortedNames.txt
+#java Sort 3000 Files/linuxwords.txt SortedNames.txt
 
 
 
